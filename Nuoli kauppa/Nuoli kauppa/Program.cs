cf0bd6bce7b2bf60323ef97b54ff8277ad5eb603
JantQ-2 <s2300891@edu.bc.fi>
2025-01-23 10:23:32 +0200

    ateria

---
+++ Nuoli kauppa/Nuoli kauppa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Nuoli_kauppa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- Ruoka-Annos/Ruoka-Annos/Program.cs
+++ Ruoka-Annos/Ruoka-Annos/Program.cs
@@ -2,9 +2,67 @@
 {
     internal class Program
     {
+        enum PääRaakaAine
+        {
+            Liha,
+            Kana, 
+            Kasviksia
+        }
+        enum Lisuke
+        {
+            Peruna,
+            Riisi,
+            Pasta,
+        }
+        enum Kastike
+        {
+            Pippuri,
+            Curry,
+            Chili
+        }
+
+        class Ateria
+        {
+            private PääRaakaAine pääaine;
+            private Lisuke lisuke;
+            private Kastike kastike;
+
+            public Ateria(PääRaakaAine pääaine, Lisuke lisuke, Kastike kastike)
+            {
+                this.pääaine = pääaine;
+                this.lisuke = lisuke;  
+                this.kastike = kastike;
+            }
+            public Kastike AnnaKastike()
+            {
+                return this.kastike;
+            }
+
+            public override string ToString()
+            {
+                return $"Pääraaka-aine: {pääaine}, lisuke: {lisuke}, kastike: {kastike}";
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Valitse pääraaka-aine: ");
+            Console.WriteLine("0 - Liha, 1 - Kana, 2 - Kasvikset");
+            PääRaakaAine valittuPääAine = (PääRaakaAine)int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valitse lisuke: ");
+            Console.WriteLine("0 - Peruna, 1 - Riisi, 2 - Pasta");
+            Lisuke valittuLisuke = (Lisuke)int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valitse kastike: ");
+            Console.WriteLine("0 - Curry, 1 - Hapanimelä, 2 - Pippuri, 3 - Chili");
+            Kastike valittuKastike = (Kastike)int.Parse(Console.ReadLine());
+
+            Ateria tilaus = new Ateria(valittuPääAine, valittuLisuke, valittuKastike);
+
+            Console.WriteLine($"Olet valinnut: {tilaus}");
+
         }
+
     }
 }
