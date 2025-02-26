1884d4abe4eceffd4f49431714833b39be061524
JantQ-2 <s2300891@edu.bc.fi>
2025-02-26 12:24:56 +0200

    Harjoitus 1

---
+++ Unity jutskat/Interface/Harjoitus_1/mouse.cs
@@ -0,0 +1,23 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class mouse : MonoBehaviour, IDamageable
+{
+    public GameObject effect;
+    public int health = 20;
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            if (effect)
+            {
+                Instantiate(effect, transform.position, Quaternion.identity);
+
+            }
+        }
+    }
+}
